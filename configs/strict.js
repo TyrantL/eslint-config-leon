fca50c151f12a74c988026e15682a1d81744bc73
lzez <lzez@servyou.com.cn>
2024-03-29 17:46:19 +0800

    feat(ecma): eslint config for strict

---
+++ configs/strict.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    // Require or disallow strict mode directives
+    // https://zh-hans.eslint.org/docs/latest/rules/strict
+    strict: ['error', 'never'],
+  },
+};
