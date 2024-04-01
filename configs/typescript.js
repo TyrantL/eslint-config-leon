9cfb872178be2f903e0aa6101b358fcfa26ce887
lzez <lzez@servyou.com.cn>
2024-04-01 09:26:09 +0800

    feat(ecma): eslint config for typescript

---
+++ configs/typescript.js
@@ -0,0 +1,27 @@
+const IS_DEVELOPMENT = require('../lib/utils/index');
+
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': IS_DEVELOPMENT ? 'warn' : 'error',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'no-undef': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/type-annotation-spacing': 'error',
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+  },
+};
